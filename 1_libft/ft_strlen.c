13f61ba27f6c02339afdc8a39094837943a6ab0b
Federico Webe Ireill <fwebe-ir@student.42perpignan.fr>
2024-11-07 12:00:49 +0100

    strlen tested with extentions created

---
+++ 1_libft/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 09:45:20 by fwebe-ir          #+#    #+#             */
+/*   Updated: 2024/11/07 11:56:52 by fwebe-ir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	test[] = "takin";;
+	int		test2;
+
+	test2 = 0;	
+	printf("%d", ft_strlen(test));
+	return (0);
+}
+*/
