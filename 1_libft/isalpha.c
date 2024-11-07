a748a10f2028ca9fac1952e9ba29915c58ff9459
Federico Webe Ireill <fwebe-ir@student.42perpignan.fr>
2024-11-07 13:12:04 +0100

    isalpha.c

--- 1_libft/isalpha.c
+++ 1_libft/isalpha.c
@@ -3,11 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   isalpha.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwebe-ir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:00:49 by fwebe-ir          #+#    #+#             */
-/*   Updated: 2024/11/06 16:24:40 by fwebe-ir         ###   ########.fr       */
+/*   Updated: 2024/11/07 13:11:17 by fwebe-ir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+The isalpha function in C is used to check whether a given character is 
+an alphabetic letter, meaning it belongs to the set 
+of uppercase (A-Z) or lowercase (a-z) letters. 
 
+- Returns a non-zero (true) value if the character is alphabetic.
+
+This function is commonly used in programs that need to validate input
+or distinguish letters from other characters. 
+It helps ensure that data contains expected types of characters,
+such as checking if a user entered only letters in a name field.
+*/
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+/*
+#include <ctype.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	test_chars[] = {'a', 'Z', '1', '$', '-'};
+    for (int i = 0; i < 5; i++) {
+        printf("isalpha(%c): %d\n", test_chars[i], isalpha(test_chars[i]));
+    }
+        for (int i = 0; i < 5; i++) {
+        printf("ft_isalpha(%c): %d\n", test_chars[i],
+		ft_isalpha(test_chars[i]));
+    }
+    return (0);
+}
+*/
