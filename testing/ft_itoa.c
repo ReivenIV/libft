e0e332f7b6899301b47b6d44ba69173ead7f2a7c
ReivenIV <runyalen88@duck.com>
2024-11-19 13:16:16 +0200

    itoa work in process

--- testing/ft_itoa.c
+++ testing/ft_itoa.c
@@ -12,7 +12,56 @@
 
 // char *ft_itoa(int n)
 
-int n_len(int n)
+static int  n_len(long n);
+static char	*new_str(long n_len);
+
+
+
+
+static char	*new_str(long n_len)
 {
-    
+	char	*tmp;
+
+	tmp = malloc((n_len + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	tmp[0] = '0';
+	return (tmp);
 }
+
+
+// We count how many numbers we have in n (necesary for malloc)
+static int	n_len(long n)
+{
+	int count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		count++;
+		n = n * -1;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+//! test n_len function
+// // #include <stdio.h>
+// // int main() {
+// //     // Test cases
+// //     long test_numbers[] = {0, 1, -1, 123, -123, 987654321, -987654321};
+// //     int num_tests = sizeof(test_numbers) / sizeof(test_numbers[0]);
+
+// //     // Run test cases
+// //     for (int i = 0; i < num_tests; i++) {
+// //         long n = test_numbers[i];
+// //         printf("n: %ld, n_len: %d\n", n, n_len(n));
+// //     }
+
+// //     return 0;
+// // }
